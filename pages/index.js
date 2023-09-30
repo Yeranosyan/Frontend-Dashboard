1a7357d556af98d745076342ac875c3c4c58a123
Gago Yerevan <karlenovich@yahoo.com>
2023-09-29 18:10:26 -0700

    change favicon

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Dashboard | React</title>
+        <title>Dashboard | React.js</title>
         <meta name="description" content="Dashboard using Next.js, React.js" />
+        <meta name="author" content="Gagik Yeranosyan" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
