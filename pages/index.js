8233e77a1ba64046bbcd3f1339df4916d5cd5655
Gago Yerevan <karlenovich@yahoo.com>
2023-09-29 17:56:59 -0700

    fix description in meta tag

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       <Head>
         <title>Dashboard | React</title>
-        <meta name="description" content="Dashboard using Nex.js, Reac.js" />
+        <meta name="description" content="Dashboard using Next.js, React.js" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
